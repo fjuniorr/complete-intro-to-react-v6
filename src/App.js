42afaee9535cddde2f60b19e4d873b23364009d4
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2022-03-10 19:10:02 -0300

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, props.name),
-      React.createElement("h2", {}, props.animal),
-      React.createElement("h2", {}, props.breed),
-    ]);
-  };
+import React from 'react'
+import ReactDOM from 'react-dom'
+import report from './data.json';
+import 'frictionless-components/lib/styles/report.css'
+import { Report } from 'frictionless-components/lib/components/Report'
+import { render } from 'frictionless-components/lib/render'
+
+console.log("report", report)
+console.log("Report", Report);
+
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//       React.createElement("h1", {}, props.name),
+//       React.createElement("h2", {}, props.animal),
+//       React.createElement("h2", {}, props.breed),
+//     ]);
+//   };
   
-  const App = () => {
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-    ]);
-  };
+//   const App = () => {
+//     return React.createElement("div", {}, [
+//       React.createElement("h1", {}, "Adopt Me!"),
+//       React.createElement(Pet, {
+//         name: "Lun",
+//         animal: "Dog",
+//         breed: "Havanese",
+//       }),
+//       React.createElement(Pet, {
+//         name: "Pepper",
+//         animal: "Bird",
+//         breed: "Cockatiel",
+//       }),
+//       React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+//     ]);
+//   };
   
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+//   ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+
+// const App = () => {
+//     return React.createElement(
+//       "div",
+//       {},
+//       React.createElement("h1", {}, "Adopt Me!")
+//     );
+//   };
+  
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+// ReactDOM.render(React.createElement(Report), {report}, document.getElementById("root"));
+
+render(Report, {report}, document.getElementById("root"));
